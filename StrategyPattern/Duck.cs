9ff5419e98f4ff35945ff7d4e294b6c1d23ba327
Yuriy <yuri.gonchar@gmail.com>
2021-11-30 17:31:45 +0200

    Add project files.

---
+++ StrategyPattern/Duck.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public interface FlyBehavior
+    {
+        public void fly();
+    }
+
+    public interface QuackBehavior
+    {
+        public void quack();
+    }
+
+    public abstract class Duck
+    {
+        public FlyBehavior flyBehavior;
+        public QuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public abstract void display();
+
+
+    }
+
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            flyBehavior = new FlyWithWings();
+            quackBehavior = new Quack();
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("I’m a real Mallard duck");
+        }
+    }
+
+    public class FlyWithWings : FlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I’m flying!!");
+        }
+    }
+
+    public class FlyNoWay : FlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I can’t fly");
+        }
+    }
+
+    public class Quack : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+
+    public class MuteQuack : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("<<Silence>>");
+        }
+    }
+
+    public class Squeak : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create Duck mallard = new MallardDuck");
+            Duck mallard = new MallardDuck();
+
+            Console.WriteLine("Do quack (mallard.quackBehavior.quack)");
+            mallard.quackBehavior.quack();
+
+            Console.WriteLine("Do fly (mallard.quackBehavior.fly)");
+            mallard.flyBehavior.fly();
+
+            Console.WriteLine("The End!");
+        }
+    }
+}
