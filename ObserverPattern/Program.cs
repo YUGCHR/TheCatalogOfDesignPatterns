3017d9a7f5e746de2e8a02ad45497d15b11dfb28
Yuriy <yuri.gonchar@gmail.com>
2021-12-03 14:14:58 +0200

    ObserverPattern was added

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Observer Design Pattern
+// Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.
+// Encapsulate the core (or common or engine) components in a Subject abstraction, and the variable (or optional or user interface) components in an Observer hierarchy.
+// The "View" part of Model-View-Controller.
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+// Strategy Design Pattern
+// Define a family of algorithms, encapsulate each one, and make them interchangeable.
+// Strategy lets the algorithm vary independently from the clients that use it.
+// Capture the abstraction in an interface, bury implementation details in derived classes.
+
 namespace StrategyPattern
 {
     class Program
